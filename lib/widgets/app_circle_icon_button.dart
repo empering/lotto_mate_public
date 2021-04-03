db5f11ef7d4163990437106807d6cbd7f14eab0d
empering <empering@naver.com>
2021-04-03 23:08:43 +0900

    app_circle_icon_button 속성 추가
    - iconSize, splashRadius, splashColor

--- lib/widgets/app_circle_icon_button.dart
+++ lib/widgets/app_circle_icon_button.dart
@@ -5,26 +5,34 @@ class AppCircleIconButton extends StatelessWidget {
   final Color backgroundColor;
   final Color iconColor;
   final Icon icon;
+  final double iconSize;
+  final double? splashRadius;
+  final Color splashColor;
   final VoidCallback onPressed;
 
   AppCircleIconButton(
       {this.backgroundColor = Colors.transparent,
       this.iconColor = AppColors.primary,
+      this.iconSize = 24.0,
+      this.splashRadius,
+      this.splashColor = Colors.transparent,
       required this.icon,
       required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
+    return Ink(
+      decoration: ShapeDecoration(
+        shape: CircleBorder(),
         color: backgroundColor,
       ),
       child: IconButton(
         onPressed: onPressed,
         icon: icon,
+        iconSize: this.iconSize,
         color: iconColor,
-        splashColor: backgroundColor,
+        splashColor: splashColor,
+        splashRadius: splashRadius,
       ),
     );
   }
