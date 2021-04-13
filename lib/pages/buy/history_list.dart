e5e25453cf7dfcecb49539b1f67d5b0b7f122f0d
empering <empering@naver.com>
2021-04-13 18:08:25 +0900

    나의 히스토리 광고 추가

--- lib/pages/buy/history_list.dart
+++ lib/pages/buy/history_list.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:lotto_mate/commons/app_box_decoration.dart';
 import 'package:lotto_mate/commons/app_colors.dart';
 import 'package:lotto_mate/models/buy.dart';
 import 'package:lotto_mate/pages/buy/history_view.dart';
+import 'package:lotto_mate/states/banner_ad_provider.dart';
 import 'package:lotto_mate/states/buy_history_state.dart';
 import 'package:lotto_mate/widgets/app_app_bar.dart';
 import 'package:lotto_mate/widgets/app_indicator.dart';
@@ -15,51 +17,69 @@ class HistoryList extends StatelessWidget {
   Widget build(BuildContext context) {
     context.read<BuyHistoryState>().getBuys();
 
-    return Consumer<BuyHistoryState>(
-      builder: (_, buyHistoryState, __) {
-        return Scaffold(
-          appBar: AppAppBar('나의 로또 히스토리'),
-          body: Container(
-            child: buyHistoryState.buys.length > 0
-                ? this._makeHistoryListView(buyHistoryState)
-                : Center(child: AppIndicator()),
-          ),
-        );
-      },
+    var adProvider = context.read<BannerAdProvider>();
+
+    return Scaffold(
+      appBar: AppAppBar('나의 로또 히스토리'),
+      body: Container(child: this._makeHistoryListView(adProvider)),
     );
   }
 
-  _makeHistoryListView(BuyHistoryState buyHistoryState) {
-    var buys = buyHistoryState.buys;
+  _makeHistoryListView(BannerAdProvider adProvider) {
+    return Consumer<BuyHistoryState>(builder: (_, buyHistoryState, __) {
+      var buys = buyHistoryState.buys;
+      if (buys.length == 0) {
+        return Center(child: AppIndicator());
+      }
 
-    return ListView.separated(
-      controller: buyHistoryState.listViewController,
-      padding: const EdgeInsets.all(10.0),
-      separatorBuilder: (context, index) => Divider(),
-      itemCount: buys.length + 1,
-      itemBuilder: (context, index) {
-        if (index < buys.length) {
-          return Container(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            decoration: AppBoxDecoration().circular(),
-            child: ListTile(
-              leading: _makeBuyListLeading(buys[index]),
-              title: _makeBuyListViewTitle(buys[index]),
-              dense: true,
-              onTap: () {
-                Get.to(HistoryView(buys[index]));
-              },
-            ),
-          );
-        }
+      int adCount = 0;
+      List<BannerAd> ads = [];
+      return ListView.separated(
+        controller: buyHistoryState.listViewController,
+        padding: const EdgeInsets.all(10.0),
+        separatorBuilder: (context, index) => Divider(),
+        itemCount: buys.length + 1 + adCount,
+        itemBuilder: (context, index) {
+          if (index < buys.length + adCount) {
+            if (index % 10 == 5) {
+              if (ads.length <= adCount) {
+                ads.add(adProvider.newAd);
+              }
 
-        if (buyHistoryState.hasMore) {
-          return Center(child: AppIndicator());
-        } else {
-          return Center(child: Text('더 이상 데이터가 없습니다'));
-        }
-      },
-    );
+              return Container(
+                alignment: Alignment.center,
+                decoration: AppBoxDecoration(
+                  color: Colors.white,
+                  shdowColor: Colors.transparent,
+                ).circular(),
+                child: AdWidget(ad: ads[adCount++]),
+                height: 72.0,
+              );
+            }
+
+            var buy = buys[index - (index / 10).round()];
+            return Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              decoration: AppBoxDecoration().circular(),
+              child: ListTile(
+                leading: _makeBuyListLeading(buy),
+                title: _makeBuyListViewTitle(buy),
+                dense: true,
+                onTap: () {
+                  Get.to(HistoryView(buy));
+                },
+              ),
+            );
+          }
+
+          if (buyHistoryState.hasMore) {
+            return Center(child: AppIndicator());
+          } else {
+            return Center(child: Text('더 이상 데이터가 없습니다'));
+          }
+        },
+      );
+    });
   }
 
   _makeBuyListLeading(Buy buy) {
